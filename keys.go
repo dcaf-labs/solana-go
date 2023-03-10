ce7035210f95a0fc3968160e7518b8f20edbe1cb
Mocha Dcaf <mocha@dcaf.so>
2023-03-09 23:21:45 -0500

    feat: implement scanner and valuer interface for PublicKey

--- keys.go
+++ keys.go
@@ -23,6 +23,7 @@ import (
 	"crypto/ed25519"
 	crypto_rand "crypto/rand"
 	"crypto/sha256"
+	"database/sql/driver"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -114,6 +115,34 @@ func (p PublicKey) Verify(message []byte, signature Signature) bool {
 }
 
 type PublicKey [PublicKeyLength]byte
+
+// Scan assigns a value from a database driver.
+func (p *PublicKey) Scan(value any) error {
+	if value == nil {
+		return p.Set("")
+	}
+	if convertValue, err := driver.String.ConvertValue(value); err == nil {
+		switch s := convertValue.(type) {
+		case string:
+			if err := p.Set(s); err == nil {
+				return nil
+			}
+		case []byte:
+			if err := p.Set(string(s)); err == nil {
+				return nil
+			}
+		}
+	}
+	return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type %T", value, p)
+}
+
+// Value returns a driver Value.
+func (p *PublicKey) Value() (driver.Value, error) {
+	if p == nil {
+		return nil, nil
+	}
+	return p.String(), nil
+}
 
 func PublicKeyFromBytes(in []byte) (out PublicKey) {
 	byteCount := len(in)
